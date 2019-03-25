b9dbe0169fa42b22dcbe490609672c815a49bd61
Aditya Pathre <36322026+apathre@users.noreply.github.com>
2019-03-25 19:21:25 +0100

    Updated version- computer think
    New version- Now computer can think. However, linking this with other components pending.

--- script.js
+++ script.js
@@ -34,39 +34,92 @@ function addMark(e){
     patternsX.push(parseInt(box.classList.item(1))+1);
     //console.log(patternsX);
     patternsX.sort();
-    console.log('out of func:'+patternsX);
+    //console.log('out of func:'+patternsX);
     findCombos=checkCombinations(patternsX,3);
-    console.log(JSON.stringify(findCombos));
-    console.log('only 2nd element'+findCombos[2]);
+    //console.log(JSON.stringify(findCombos));
+    //console.log('only 2nd element'+findCombos[2]);
     win=findWin(findCombos,winningCombinations);
     if(win==1){
       console.log('He won');
     }
     
+    cOptions=compTurn();
+    compThink(cOptions);
+
+    
 }
+
+function compTurn(){
+    let compTrn=[]; 
+    gamebox.forEach((ck)=>{
+     
+      if(!ck.classList.contains('selected')){
+        //console.log(ck.classList.item(1));
+        compTrn.push(parseInt(ck.classList.item(1))+1);
+        
+
+      }
+    });
+    console.log('compTurn:'+compTrn);  
+    return compTrn;
+  
+  }
+
+  function compThink(options){
+    let xMatch=[];
+    let cCombos=[];   
+    let newxMatch=[];  
+      gamebox.forEach((tm)=>{
+        if(tm.classList.contains('X')){
+          xMatch.push(parseInt(tm.classList.item(1))+1);
+          xMatch.sort();
+                   
+        }
+      });
+  
+      console.log('xMatch:'+xMatch);
+      for(let a=0;a<options.length;a++){
+         xMatch.push(parseInt(options[a]));
+         let newxMatch=xMatch;
+        // console.log('new xMatch:'+xMatch);
+         cCombos=checkCombinations(newxMatch,3);
+        // console.log("ccombos:"+cCombos);
+         //console.log("combos1:"+cCombos[1]);
+         cresult=findWin(cCombos,winningCombinations);
+         console.log("cresult:"+cresult+'at option:'+options[a]);
+         if(cresult==true){
+           gamebox.forEach((bt)=>{
+             console.log('bt item:'+bt.classList.item(1));
+             im=parseInt(bt.classList.item(1))+1;
+             console.log(im);
+             if((parseInt(bt.classList.item(1))+1)==options[a]){
+               gamebox[bt.classList.item(1)].innerText=gamePiece[1];
+             }
+
+           });
+         }
+      //console.log("xmatch before splice:"+xMatch);
+      xMatch=xMatch.filter((tl)=>{
+         return (tl!=options[a]);
+      });
+      //console.log("xmatch after filter:"+xMatch);
+      }
+        
+  }
 
 function checkCombinations(xStreak,r){
     let result=[];
     let data=[];
     let index=0;
-    //console.log('xStreak:'+xStreak);
+    xStreak.sort();
     for(let i=0;i<xStreak.length-r+1;i++){
-      //console.log('results'+result);
-      //console.log('i'+xStreak[i]);
       result.push(xStreak[i]);
-      //console.log('result after push i:'+result);
       for(let j=i+1;j<xStreak.length-r+2;j++){
         result.push(xStreak[j]);
-        //console.log('result after j push:'+result);
         for(let k=j+1;k<xStreak.length-r+3;k++){
             result.push(xStreak[k]);
-            //console.log('result after k push:'+result);
             data[index]=result.concat();
             result.splice(2,result.length);
-            //console.log('data[0]:'+ data[0]);
-            //console.log('data[1]'+data[1]);
-            //console.log('data[2]'+data[2]);
-            //console.log('data:'+index+':=> '+data[index]);
             index++;
 
         }
@@ -74,23 +127,17 @@ function checkCombinations(xStreak,r){
       }      
         result.splice(0,result.length);
     }
-     
-    return (data); 
+     return (data); 
 }
 
 function findWin(checkArray,fixArray){
-  //checkArray.join("");
-  //console.log(resArray);
-  //console.log('checkarray:'+checkArray[0]);
   let r=[];
+  
   for(let a=0;a<checkArray.length;a++){
     for(let b=0;b<fixArray.length;b++){
       r=checkArray[a].join("");
-
-      console.log('deft:'+r);
-      console.log('res array:'+fixArray);
-      if(fixArray[b]==r){
-        console.log('if loop works');
+       if(fixArray[b]==r){
+        //console.log('if loop works');
         return true;
       }
     }
